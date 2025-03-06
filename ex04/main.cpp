f31c8522343cbe16dbe72ea968781a4727fb6b80
ismail el azzouzi <ismailazzouzi.contact@gmail.com>
2025-03-06 12:56:06 +0000

    ex04 done ex05 done

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -2,12 +2,31 @@
 
 static int isSpace(std::string str)
 {
+    size_t len = 0;
     for (size_t i = 0; i < str.length(); i++)
     {
         if ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
-            return (1);
+            len++;
     }
+    if (len == str.length())
+        return (1);
     return (0);
+}
+
+
+static std::string replaceFileContent(std::string content, std::string s1, std::string s2)
+{
+    std::string result;
+    
+    result = content;
+    size_t position = 0;
+    while ((position = result.find(s1, position)) != std::string::npos)
+    {
+        result.erase(position, s1.length());
+        result.insert(position, s2);
+        position += s2.length();
+    }
+    return (result);
 }
 
 int main(int ac, char **av)
@@ -23,8 +42,7 @@ int main(int ac, char **av)
                 std::cout << "Invalid Arguments !" << std::endl;
                 return (1);
             }
-            av++;    
-            std::cout << newAv[i] << std::endl;
+            av++;
         }
         av -= ac;
         std::ifstream   file(newAv[1].c_str());
@@ -33,6 +51,23 @@ int main(int ac, char **av)
             std::cout << "file doesn't exist or cannot be opened !" << std::endl;
             return (1);
         }
+        std::string content;
+        std::string line;
+        while (getline(file, line))
+            content += line + "\n";
+        if (content.empty())
+        {
+            std::cout << "file is empty !" << std::endl;
+            return (1);
+        }
+        content.pop_back();
+        file.close();
+        std::string newContent = replaceFileContent(content, newAv[2], newAv[3]);
+        std::cout << newContent << std::endl;
+        std::string result = newAv[1] + ".replace";
+        std::ofstream ofs(result, std::ofstream::out);
+        ofs << newContent;
+        ofs.close();
     }
     else
     {
---
+++ ex05/main.cpp
@@ -0,0 +1,12 @@
+#include "Harl.hpp"
+
+int main(void)
+{
+    Harl harl;
+
+    harl.complain("WARNING");
+    harl.complain("DEBUG");
+    harl.complain("ERROR");
+    harl.complain("INFO");
+    harl.complain("DOLPHINS");
+}
